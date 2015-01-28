567e04d28ab8def5f31f9152a8b109f4650b9a6b
hongkunyoo <hongkunyoo@gmail.com>
2015-01-28 22:31:47 +0900

    it-em_0.01_001

--- It-em/src/com/pinthecloud/item/GlobalVariable.java
+++ It-em/src/com/pinthecloud/item/GlobalVariable.java
@@ -1,6 +1,6 @@
 package com.pinthecloud.item;
 
 public class GlobalVariable {
-	public static final boolean DEBUG_MODE = true;
+	public static final boolean DEBUG_MODE = false;
 	public static final String GOOGLE_PLAY_SERVICE_APP_ID = "com.google.android.gms";
 }
