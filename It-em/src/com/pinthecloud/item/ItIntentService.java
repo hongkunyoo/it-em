e32c38a65fab68b0f88f5f442c8919fd2cb2f4c4
hongkunyoo <hongkunyoo@gmail.com>
2015-02-05 00:51:03 +0900

    almost finished GCm

--- It-em/src/com/pinthecloud/item/ItIntentService.java
+++ It-em/src/com/pinthecloud/item/ItIntentService.java
@@ -2,13 +2,20 @@ package com.pinthecloud.item;
 
 import java.util.List;
 
-import com.pinthecloud.item.util.ItLog;
-
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningTaskInfo;
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.AudioManager;
+import android.os.Vibrator;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+
+import com.pinthecloud.item.util.ItLog;
 
 public class ItIntentService extends IntentService {
 
@@ -35,68 +42,70 @@ public class ItIntentService extends IntentService {
 		String unRegisterd = intent.getStringExtra("unregistered");
 		if (unRegisterd != null && unRegisterd.equals(GlobalVariable.GOOGLE_PLAY_APP_ID)) return;
 		
-		ItLog.log(intent.getExtras(), intent);
-//		String messageStr = intent.getExtras().getString("message");
+		String message = intent.getExtras().getString("message");
+		
+		ItLog.log(message);
+		alertNotification(message);
 	}
 
 
 	/**
 	 *  Method For alerting notification
 	 */
-	//	private void alertNotification(){
-	//		/*
-	//		 * Creates an explicit intent for an Activity in your app
-	//		 */
-	//		Intent resultIntent = new Intent();
-	//		String title = "";
-	//		String content = "";
-	//		Resources resources = _this.getResources();
-	//
-	//		// The stack builder object will contain an artificial back stack for the
-	//		// started Activity.
-	//		// This ensures that navigating backward from the Activity leads out of
-	//		// your application to the Home screen.
-	//		TaskStackBuilder stackBuilder = TaskStackBuilder.create(_this);
-	//
-	//		// Adds the back stack for the Intent (but not the Intent itself)
-	//		if (AhMessage.TYPE.CHUPA.equals(type)){
-	//			stackBuilder.addParentStack(ChupaChatActivity.class);
-	//		}
-	//
-	//		// Adds the Intent that starts the Activity to the top of the stack
-	//		stackBuilder.addNextIntent(resultIntent);
-	//
-	//		// Set intent and bitmap
-	//		PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT);
-	//		Bitmap bitmap = null;
-	//		if(user != null){
-	//			bitmap = FileUtil.getBitmapFromInternalStorage(app, user.getId()+AhGlobalVariable.SMALL);
-	//		}else{
-	//			bitmap = BitmapUtil.decodeInSampleSize(getResources(), R.drawable.launcher, BitmapUtil.SMALL_PIC_SIZE, BitmapUtil.SMALL_PIC_SIZE);
-	//		}
-	//
-	//
-	//		/*
-	//		 * Set Notification
-	//		 */
-	//		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(_this)
-	//		.setSmallIcon(R.drawable.launcher)
-	//		.setLargeIcon(bitmap)
-	//		.setContentTitle(title)
-	//		.setContentText(content)
-	//		.setAutoCancel(true);
-	//		mBuilder.setContentIntent(resultPendingIntent);
-	//
-	//		// Notify!
-	//		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-	//		mNotificationManager.notify(1, mBuilder.build());
-	//
-	//		// For Vibration
-	//		AudioManager audioManager = (AudioManager) _this.getSystemService(Context.AUDIO_SERVICE);
-	//		if(AudioManager.RINGER_MODE_SILENT != audioManager.getRingerMode()){
-	//			((Vibrator)getSystemService(Context.VIBRATOR_SERVICE)).vibrate(800);
-	//		}
-	//	}
+		private void alertNotification(String message){
+			/*
+			 * Creates an explicit intent for an Activity in your app
+			 */
+			Intent resultIntent = new Intent();
+			String title = "";
+			String content = message;
+	
+			// The stack builder object will contain an artificial back stack for the
+			// started Activity.
+			// This ensures that navigating backward from the Activity leads out of
+			// your application to the Home screen.
+			TaskStackBuilder stackBuilder = TaskStackBuilder.create(mThis);
+	
+			// Adds the back stack for the Intent (but not the Intent itself)
+//			if (AhMessage.TYPE.CHUPA.equals(type)){
+//				stackBuilder.addParentStack(ChupaChatActivity.class);
+//			}
+	
+			// Adds the Intent that starts the Activity to the top of the stack
+			stackBuilder.addNextIntent(resultIntent);
+	
+			// Set intent and bitmap
+			PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT);
+			
+			Bitmap bitmap = null;
+//			if(user != null){
+//				bitmap = FileUtil.getBitmapFromInternalStorage(app, user.getId()+AhGlobalVariable.SMALL);
+//			}else{
+//				bitmap = BitmapUtil.decodeInSampleSize(getResources(), R.drawable.launcher, BitmapUtil.SMALL_PIC_SIZE, BitmapUtil.SMALL_PIC_SIZE);
+//			}
+	
+	
+			/*
+			 * Set Notification
+			 */
+			NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(mThis)
+			.setSmallIcon(R.drawable.launcher)
+//			.setLargeIcon(bitmap)
+			.setContentTitle(title)
+			.setContentText(content)
+			.setAutoCancel(true);
+			mBuilder.setContentIntent(resultPendingIntent);
+	
+			// Notify!
+			NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+			mNotificationManager.notify(GlobalVariable.NOTIFICATION_ID, mBuilder.build());
+	
+			// For Vibration
+			AudioManager audioManager = (AudioManager) mThis.getSystemService(Context.AUDIO_SERVICE);
+			if(AudioManager.RINGER_MODE_SILENT != audioManager.getRingerMode()){
+				((Vibrator)getSystemService(Context.VIBRATOR_SERVICE)).vibrate(800);
+			}
+		}
 
 
 	/**
