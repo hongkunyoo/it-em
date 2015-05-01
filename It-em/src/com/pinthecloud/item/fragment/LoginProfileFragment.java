867883fb2e5f906ee97b3ae039847b43571c24f6
Seungmin <rfrost77@gmail.com>
2015-05-01 18:47:12 +0900

    I started user information logic.

---
+++ It-em/src/com/pinthecloud/item/fragment/LoginProfileFragment.java
@@ -0,0 +1,22 @@
+package com.pinthecloud.item.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pinthecloud.item.R;
+
+public class LoginProfileFragment extends ItFragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		View view = inflater.inflate(R.layout.fragment_login_profile, container, false);
+
+		mGaHelper.sendScreen(mThisFragment);
+
+		return view;
+	}
+}
